17df20edde1e9a7f4e190d53718aa0876e2a6ece
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-05-08 20:32:21 -0300

    versão 1.0
    lógica pronta e funcional

--- js/jodo-da-memória/projeto/script.js
+++ js/jodo-da-memória/projeto/script.js
@@ -2,10 +2,8 @@ let sessaoJogo = document.querySelector('.game')
 let carta1 = ''
 let carta2 = ''
 let pontos = 0
+let acertos = []
 
-function play(){
-    pegarDados()
-}
 
 //pegar dados e criar as cartas
 const url = 'cards.json'
@@ -23,23 +21,24 @@ function pegarDados() {
             // Limpa a sessão p criar as cartas
             sessaoJogo.innerHTML = ""
 
-            // Crie as cartas dinamicamente
+            // Crie as cartas
             dados.cartas.forEach(cartas => {
-            gerarCartas(cartas);
+            gerarCartas(cartas)
         });
     })
-    .catch(error => console.log("Erro ao carregar JSON: ", error));
+    .catch(error => console.log("Erro ao carregar JSON: ", error))
 }
 
-//criação das cartas
+
 function gerarCartas(cartas){
     let carta = document.createElement("div")
     carta.setAttribute('class', 'card') 
-   
-     
-    // carta.textContent = cartas.nome
     sessaoJogo.appendChild(carta)
 
+    //manter cartas viradas quando acertadas
+    if (acertos.includes(cartas.cor)) {
+        carta.style.background = cartas.cor
+    }
 
     //click
     carta.addEventListener('click', () => {
@@ -51,25 +50,37 @@ function gerarCartas(cartas){
             carta.classList.toggle('virada')
             setTimeout(() => {
             carta.style.background = cartas.cor
-
         }, 550)
-        //comparação
         
         if (carta1 === '') {
             carta1 = cartas.cor
             console.log('carta1: ' + carta1)
-
-        } else if (carta1 === cartas.cor){
-            pontos ++
-            console.log('cartas iguais, vc tem '  + pontos + 'pontos')
-            carta1 = ''
-        }
-        else {
-            console.log('errada')
+         } 
+        else if (carta1 != ''){
+            carta2 = cartas.cor
+            console.log ('carta2: '+ carta2)
+            comparar(carta1, carta2)
         }
         }
-
     })
 }
+function comparar(c1, c2){
+    if (c1 === c2){ 
+        pontos ++ 
+        acertos.push(c1)
+        console.log ('cartas iguais')
+        console.log ('pontuação: ' + pontos)
+        console.log (acertos)
+        carta1 = ''
+        carta2 = ''
+    }
+    else{
+        console.log ('cartas diferentes')
+        carta1 = ''
+        carta2 = ''
+    }
+    setTimeout(() => {
+        pegarDados()
+    },2000)
+}
 
-//definir carta fora da função pq ta dando erro na hora de reiniciar as cartas todo o resto ta ok

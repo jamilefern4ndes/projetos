a0d93f3b701692101ec95eeeb3adbb27331d8f10
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-05-24 15:36:04 -0300

    v1.0

--- js/lista-de-tarefas/script.js
+++ js/lista-de-tarefas/script.js
@@ -1,5 +1,36 @@
+function enter(event){
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        addTask()
+    }
+}
+let inputTask = document.querySelector('#task')
+let taskArea = document.querySelector('.taskArea')
+
+let itens = []
 
 function addTask(){
+
     let newTask = document.querySelector('#task').value
     console.log ('nova tarefa: ' + newTask)
+    itens.push(newTask)
+    let item = document.createElement('label')
+    item.setAttribute("id", "novoItem")
+    item.innerHTML = 
+        `<input type="checkbox" name="${newTask}"
+        <p>${newTask}</p>
+        <div id="delete">x</div>`
+    taskArea.appendChild(item)
+    inputTask.value = ''
+
+    let deleteButton = item.querySelector('#delete')
+    if (deleteButton) {
+        deleteButton.addEventListener('click', () => {
+            taskArea.removeChild(item)
+            itens = itens.filter(task => task !== newTask)
+            console.log(itens)
+        })
 }
+}
+
+
